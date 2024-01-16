fdefe365148a9e2272c7fe24e0616631ba24eebf
郑松岩(平台开发) <zhengsongyan@BG-U2204-0003.t3caic.com>
2024-01-16 17:28:02 +0800

    new

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-qemu-system-arm -M vexpress-a9 -m 512M -dtb vexpress-v2p-ca9.dtb -kernel zImage -nographic -append "root=/dev/mmcblk0 rw console=ttyAMA0 init =/linuxrc" -sd rootfs.ext3
+qemu-system-arm -M vexpress-a9 -m 512M -dtb /home/zhengsongyan/linux/tool/Arm_vexpress/vexpress-v2p-ca9.dtb -kernel /home/zhengsongyan/linux/tool/Arm_vexpress/zImage -nographic -append "root=/dev/mmcblk0 rw console=ttyAMA0 init =/linuxrc" -sd /home/zhengsongyan/linux/tool/Arm_vexpress/rootfs.ext3
